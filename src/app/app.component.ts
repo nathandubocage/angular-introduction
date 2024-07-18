49e1f4f972f53e0ef3b4ce91fd1d11102b3a8e3e
Nathan Dubocage <nathandubocage@gmail.com>
2024-07-18 10:54:56 +0200

    Nettoyage du composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  computed,
-  effect,
-  inject,
-  signal,
-  ViewChild,
-} from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonPrimaryComponent } from './button-primary/button-primary.component';
 import { CountComponent } from './count/count.component';
@@ -14,13 +7,6 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { ItemsComponent } from './items/items.component';
-
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -45,17 +31,6 @@ export class AppComponent {
 
   public testSignal = signal('Nathan');
 
-  public user = new BehaviorSubject({
-    id: 1,
-    name: 'Nathan',
-    username: 'nathan',
-    email: 'nathan@gmail.com',
-  });
-
-  public userDisplay = this.user.asObservable();
-
-  @ViewChild('loginForm') loginForm!: NgForm;
-
   constructor() {
     effect(() => {
       console.log('Compteur :', this.completedTaskCount());
@@ -64,8 +39,6 @@ export class AppComponent {
 
   public ngOnInit(): void {
     this.usersService.getUsers().subscribe();
-
-    this.user.subscribe((user) => {});
   }
 
   public up(): void {
@@ -80,9 +53,5 @@ export class AppComponent {
 
   public completeTask(): void {
     this.countSignal.update((count) => count * 2);
-  }
-
-  public onSubmit(form: NgForm): void {
-    console.log(form.value);
   }
 }
